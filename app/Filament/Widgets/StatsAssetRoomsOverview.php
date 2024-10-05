848523b8db0bbf4150aeeb7f30a79aaf4db61769
terminator791 <mohammad.iqbal7781@gmail.com>
2024-10-05 16:11:34 +0700

    Feat : add widget dashboard, fix chart

--- app/Filament/Widgets/StatsAssetRoomsOverview.php
+++ app/Filament/Widgets/StatsAssetRoomsOverview.php
@@ -11,9 +11,6 @@ class StatsAssetRoomsOverview extends BaseWidget
 {
     protected int | string | array $column = 2;
 
-    //bagaimana cara jadikan hanya 2 widget per column
-
-
 
     protected function getStats(): array
     {
@@ -28,6 +25,15 @@ class StatsAssetRoomsOverview extends BaseWidget
         $rooms_brokens = DB::table('room_assets')
             ->where('isBroken', 1)
             ->distinct('room_id')
+            ->count('room_id');
+
+        $rooms_normal = $total_kamar - $rooms_brokens;
+
+        $rooms_normal_items = DB::table('room_assets')
+            ->where('isBroken', 0)
+            ->count('room_id');
+
+        $total_items = DB::table('room_assets')
             ->count('room_id');
             
         return [
@@ -46,6 +52,24 @@ class StatsAssetRoomsOverview extends BaseWidget
 
             Stat::make('Total Item Yang Rusak', $rooms_brokens_items)
             ->description('Menampilkan Total Item Yang Rusak')
+            ->descriptionIcon('heroicon-o-clipboard-document-check')
+            ->chart([50, 89, 55, 53, 44, 80, 92])
+            ->color('Amber'),
+
+            Stat::make('Total Kamar Normal', $rooms_normal)
+            ->description('Menampilkan Total Kamar Yang Normal')
+            ->descriptionIcon('heroicon-o-clipboard-document-check')
+            ->chart([50, 89, 55, 53, 44, 80, 92])
+            ->color('cyan'),
+
+            Stat::make('Total items ', $total_items)
+            ->description('Menampilkan Total items')
+            ->descriptionIcon('heroicon-o-clipboard-document-check')
+            ->chart([50, 89, 55, 53, 44, 80, 92])
+            ->color('purple'),
+
+            Stat::make('Total items Normal', $rooms_normal_items)
+            ->description('Menampilkan Total items Yang Normal')
             ->descriptionIcon('heroicon-o-clipboard-document-check')
             ->chart([50, 89, 55, 53, 44, 80, 92])
             ->color('Amber')
